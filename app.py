5faf8a057f6e77cf7959e123e935f10e0078b823
nikhilonwords <ceo@Onwords.in>
2025-03-19 20:36:20 +0530

    chore: prepare for Render deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from ai_api import get_ai_response, clear_memory, get_conversation_history
+import os
 
 app = Flask(__name__)
 
@@ -38,4 +39,5 @@ def history():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port) 
